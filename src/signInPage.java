a032ce332c49f4062c338dbbb063382238c6004f
RSC <97985943+RMartinezGonzalez@users.noreply.github.com>
2025-09-10 13:15:08 -0600

    Add files via upload

---
+++ src/signInPage.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class signInPage {
+    private JPanel mainPanel;
+    private JLabel bImage;
+    private JPanel contentPanel;
+    private JPanel imagePanel;
+    private JLabel singInText;
+    private JPanel textPanel;
+    private JPanel fieldPanel;
+    private JTextField emailField;
+    private JTextField passwordField;
+    private JPanel rememberMePanel;
+    private JCheckBox remembeMeText;
+    private JPanel signInPanel;
+    private JButton signInButton;
+    private JPanel copyrightPanel;
+    private JLabel copyrightText;
+}
